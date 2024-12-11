f6a172e4fa4d01bf7d29aff33840299d1f9b2915
AkshayDholakiya <96297018+akshayd021@users.noreply.github.com>
2024-12-11 15:06:03 +0530

    fix

--- router/userRouter.js
+++ router/userRouter.js
@@ -16,14 +16,14 @@ router.post("/create-user", async (req, res) => {
     if (customUrl) {
       const existingUser = await User.findOne({ customUrl });
       if (existingUser) {
-        return res.status(400).json({ error: "Custom URL already taken" });
+        return res.status(400).json({ message: "Custom URL already taken" });
       }
     }
 
     const newUser = new User({ name, age, message, customUrl });
     await newUser.save();
 
-    const dummyLink = `https://birthday-cake-sigma.vercel.app/user/${
+    const dummyLink = `https://www.waiwishes.com//user/${
       customUrl || newUser._id
     }`;
 
@@ -42,28 +42,20 @@ router.post("/create-user", async (req, res) => {
 router.get("/get-user/:identifier", async (req, res) => {
   try {
     const { identifier } = req.params;
-    console.log("Received identifier:", identifier);
 
     let user;
-
-    // Check if identifier is a valid MongoDB ObjectId
     if (mongoose.Types.ObjectId.isValid(identifier)) {
-      console.log("Searching by ObjectId...");
       user = await User.findById(identifier);
     } else {
-      console.log("Searching by customUrl...");
       user = await User.findOne({ customUrl: identifier });
     }
 
     if (!user) {
-      console.log("User not found"); // Log if user is not found
-      return res.status(404).json({ error: "User not found" });
+      return res.status(404).json({ message: "User not found" });
     }
-
-    console.log("User found:", user); // Log the found user
     res.status(200).json({ user });
   } catch (error) {
-    console.error("Error occurred:", error.message); // Log the error
+    console.error("Error occurred:", error.message);
     res
       .status(500)
       .json({ error: "An error occurred while retrieving the user" });
