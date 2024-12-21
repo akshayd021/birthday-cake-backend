353cf567eb7f6b1315036dc96243d76c941f58d6
utsavvasoya8585 <71059051+utsavvasoya8585@users.noreply.github.com>
2024-12-21 16:04:31 +0530

    Done

--- controller/paymentController.js
+++ controller/paymentController.js
@@ -2,74 +2,17 @@ const crypto = require("crypto");
 const axios = require("axios");
 const sha256 = require("sha256");
 // const { , merchant_id } = require("./secret");
-const salt_key = "96434309-7796-489d-8924-ab56988a6076";
-const merchant_id = "PGTESTPAYUAT86";
-const salt_index = 1;
+// Testing enviroment
+// const salt_key = "96434309-7796-489d-8924-ab56988a6076";
+// const merchant_id = "PGTESTPAYUAT86";
+// const salt_index = 1;
+// const prod_URL = "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay";
 
-// const newPayment = async (req, res) => {
-//   try {
-//     console.log("Calling req");
-//     const merchantTransactionId = Date.now();
-//     const data = {
-//         "merchantId": "PGTESTPAYUAT86",
-//         "merchantTransactionId": "MT7850590068188104",
-//         "merchantUserId": "MUID123",
-//         "amount": 10000,
-//         "redirectUrl": "https://webhook.site/redirect-url",
-//         "redirectMode": "REDIRECT",
-//         "callbackUrl": "https://webhook.site/callback-url",
-//         "mobileNumber": "9999999999",
-//         "paymentInstrument": {
-//           "type": "PAY_PAGE"
-//         }
-//       }
-//       ;
-//     const payload = JSON.stringify(data);
-//     const payloadMain = Buffer.from(payload).toString("base64");
-//     const keyIndex = 1;
-//     const string = payloadMain + "/pg/v1/pay" + salt_key;
-//     const sha256 = crypto.createHash("sha256").update(string).digest("hex");
-//     const checksum = sha256 + "###" + keyIndex;
-
-//     // const prod_URL = "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay";
-//     const prod_URL = "https://api-preprod.phonepe.com/apis/hermes/pg/v1/pay";
-//     // const prod_URL = "https://api.phonepe.com/apis/hermes/pg/v1/pay";
-
-//     console.log("Checksum: ", checksum);
-//     console.log("Payload: ", payloadMain);
-//     const options = {
-//       method: "POST",
-//       url: prod_URL,
-//       headers: {
-//         accept: "application/json",
-//         "Content-Type": "application/json",
-//         "X-VERIFY": checksum,
-//       },
-//       data: {
-//         request: payloadMain,
-//       },
-//     };
-
-//     axios
-//       .request(options)
-//       .then(function (response) {
-//         console.log(response.data);
-//         return res.status(200);
-//         // return res.redirect(
-//         //   response.data.data.instrumentResponse.redirectInfo.url
-//         // );
-//       })
-//       .catch(function (error) {
-//         console.error(error);
-//       });
-//   } catch (error) {
-//     res.status(500)
-//     // res.status(500).send({
-//     //   message: error.message,
-//     //   success: false,
-//     // });
-//   }
-// };
+// Producttions
+const salt_key = "386318c5-8294-4517-83ae-ebcb2cf18877";
+const merchant_id = "WAIWIONLINE";
+const salt_index = 2;
+const prod_URL = "https://api.phonepe.com/apis/hermes";
 
 const newPayment = async (req, res) => {
   const name = req?.body?.name;
@@ -77,7 +20,8 @@ const newPayment = async (req, res) => {
   const age = req?.body?.age;
   const customUrl = req?.body?.customUrl;
 
-  console.log("Calling payment... ", name, msg, age, customUrl);
+  // console.log("Calling payment... ", name, msg, age, customUrl);
+  // console.log({salt_key, salt_index, merchant_id});
 
   const merchantTransactionId = "T" + Date.now();
   const data = {
@@ -105,8 +49,6 @@ const newPayment = async (req, res) => {
   const encodedPayload = bufferObj.toString("base64");
   console.log("Base 64: ", encodedPayload);
   const xVerify = sha256(encodedPayload + "/pg/v1/pay" + salt_key) + "###1";
-
-  const prod_URL = "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay";
 
   const options = {
     method: "POST",
