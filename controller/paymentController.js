8185ed05222321a81963a2dd7bdc38b14a25543f
utsavvasoya8585 <71059051+utsavvasoya8585@users.noreply.github.com>
2025-01-08 12:01:26 +0530

    Done

--- controller/paymentController.js
+++ controller/paymentController.js
@@ -37,7 +37,7 @@ const newPayment = async (req, res) => {
     merchantUserId: "MUID" + Date.now(),
     name: req?.body?.name,
     amount: 100,
-    // redirectUrl: `${backend_URL}/api/status/${merchantTransactionId}/${name}/${msg}/${age}/${customUrl}`,
+    redirectUrl: `${backend_URL}/api/status/${merchantTransactionId}/${name}/${msg}/${age}/${customUrl}`,
     redirectMode: "REDIRECT",
     callbackUrl: `${backend_URL}/api/status/${merchantTransactionId}/${name}/${msg}/${age}/${customUrl}`,
     mobileNumber: "6353839209",
--- router/PhonePayRoute.js
+++ router/PhonePayRoute.js
@@ -3,7 +3,7 @@ const { newPayment, checkStatus } = require("../controller/paymentController");
 const router = express();
 
 router.post("/payment", newPayment);
-router.get("/status/:txnId/:name/:msg/:age/:customUrl", checkStatus);
+router.post("/status/:txnId/:name/:msg/:age/:customUrl", checkStatus);
 router.get("/message", (req, res) => {
   const message = req.session.message || null; // Retrieve the message
   req.session.message = null; // Clear the message after reading
